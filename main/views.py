407ff7b6b1eab07045862f13dcddb37558ccd1f4
Артём Светилов <smart12.12@mail.ru>
2024-03-25 06:43:08 +0300

    added static files and base html for main app

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         'title': 'Home',
-        'content': 'Главная страница магазина'
+        'content': 'Главная страница магазина',
     }
     return render(request, 'main/index.html', context=context)
 
